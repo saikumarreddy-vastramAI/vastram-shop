f007906acfa60a7dd616898a366007629f435c94
Sai Kumar <saikumarreddy1000@gmail.com>
2026-05-11 20:18:22 +0400

    Fix ESLint warning for Vercel deployment

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,6 +11,7 @@ function Dashboard({ API, token, shopId, onBack }) {
 
   useEffect(() => {
     loadDashboard();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const loadDashboard = async () => {
